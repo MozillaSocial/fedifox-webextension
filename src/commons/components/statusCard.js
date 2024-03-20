3c0cb7315acf5d03765a36b346827150e20db797
Amri Toufali <amri@handsomemedia.com>
2024-03-19 22:59:36 -0700

    display media in content feed

--- src/commons/components/statusCard.js
+++ src/commons/components/statusCard.js
@@ -3,18 +3,6 @@
  * Card to display a status/post from a user's status feed/timeline
  */
 
-const html = data => `
-<article id=${data.id}>
-  <header>
-    <img src="${data.account.avatar}">
-    <address>${data.account.display_name || data.account.username}</address>
-    <time datetime="${data.created_at}">${data.created_at}</time>
-  </header>
-  <p>${data.content}</p>
-  <p>${data.media_attachments?.map(a => a.url)}</p>
-</article>
-`
-
 class StatusCard extends HTMLElement {
   #status
 
@@ -27,8 +15,38 @@ class StatusCard extends HTMLElement {
     return this.#status
   }
 
+  parseMedia(obj) {
+    const figure = document.createElement('figure')
+
+    switch (obj.type) {
+      case 'audio':
+        figure.innerHTML = `<audio src="${obj.url}" id="${obj.id}" controls></audio>`
+        break
+      case 'image':
+        figure.innerHTML = `<img src="${obj.url}" id="${obj.id}">`
+        break
+      case 'video':
+        figure.innerHTML = `<video src="${obj.url}" id="${obj.id}" controls></video>`
+        break
+    }
+
+    return figure.outerHTML
+  }
+
   render() {
-    this.innerHTML = html(this.#status)
+    const data = this.#status
+
+    this.innerHTML = `
+    <article id=${data.id}>
+      <header>
+        <img src="${data.account.avatar}">
+        <address>${data.account.display_name || data.account.username}</address>
+        <time datetime="${data.created_at}">${data.created_at}</time>
+      </header>
+      ${data.content}
+      <div class="media">${data.media_attachments?.map(obj => this.parseMedia(obj)).join('')}</div>
+    </article>
+    `
   }
 }
 
