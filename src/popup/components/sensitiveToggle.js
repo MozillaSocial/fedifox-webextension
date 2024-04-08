ee1cefa46de4189c45d0ceef1a11beda64e2e55f
Amri Toufali <amri@handsomemedia.com>
2024-04-08 15:59:08 -0700

    update sensitive-toggle style

--- src/popup/components/sensitiveToggle.js
+++ src/popup/components/sensitiveToggle.js
@@ -5,40 +5,25 @@
  */
 
 customElements.define("sensitive-toggle", class sensitiveToggle extends HTMLElement {
-  connectedCallback() {
-    this.addEventListener('click', this)
+  #dialog
+
+  connectedCallback() { 
     this.innerHTML = `
-    <style>
-      sensitive-toggle dialog{
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        gap: var(--padding-sm);
-        background: rgba(255 255 255 / .5);
-        border: 1px solid var(--light-gray);
-        border-radius: var(--border-radius);
-        -webkit-backdrop-filter: blur(24px);
-        backdrop-filter: blur(24px);
-      }
-    </style>
     <dialog open>
       <h3>Sensitive Content</h3>
       <p>${this.spoiler || 'Some people may find this content offensive or disturbing'}</p>
-      <form method="dialog">
-        <button class="secondary">Show</button>
-      </form>
+      <button class="secondary">Show</button>
     </dialog>
     `
+    this.#dialog = this.querySelector('dialog')
+    this.addEventListener('pointerdown', this)
+  }
+
+  disconnectedCallback() {
+    this.removeEventListener('pointerdown', this)
   }
 
   handleEvent(e) {
-    console.log('handle sensitiveToggle event')
-    // this.querySelector('dialog').show()
+    if (this.#dialog.open) this.#dialog.close()
   }
 })
